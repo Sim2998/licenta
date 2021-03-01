f9c3685a8c1652ffcaf6597f390121b31069ac2a
Sim Bordanca <sim_bordanca@yahoo.com>
2021-03-01 17:56:42 +0200

    modified FAR and stages count

--- train.m
+++ train.m
@@ -1,7 +1,7 @@
 negativeFolder = 'no_mask'
-NumStages = 14;
+NumStages = 20;
 FAR = 0.6;
 
 masks = [m.DataSource.Source m.LabelData];
 masks = masks(:,[1,2]);
-trainCascadeObjectDetector('maskdetectorr_14_60.xml', masks, negativeFolder, 'NumCascadeStages', NumStages, 'FalseAlarmRate', FAR);
+trainCascadeObjectDetector('maskdetectorr_20_60.xml', masks, negativeFolder, 'NumCascadeStages', NumStages, 'FalseAlarmRate', FAR);
