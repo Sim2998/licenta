c83aba17cc164416eff48c56dc318ec791af0ad6
Sim Bordanca <sim_bordanca@yahoo.com>
2021-03-10 21:24:56 +0200

    improved

--- detect_masks.m
+++ detect_masks.m
@@ -1,38 +1,29 @@
 detector = vision.CascadeObjectDetector('maskdetectorr_20_60.xml');
-nose_detector = vision.CascadeObjectDetector('Nose', 'MergeThreshold', 20);
-mouth_detector = vision.CascadeObjectDetector('Mouth','MergeThreshold', 110);
+nose_detector = vision.CascadeObjectDetector('Nose', 'MergeThreshold', 20, 'UseROI', true);
+mouth_detector = vision.CascadeObjectDetector('Mouth','MergeThreshold', 110, 'UseROI', true);
 counter = 0;
 for k = 800:854
     filename = ['images\' num2str(k) '.jpg'];
     I = imread(filename);
-    release(nose_detector);
-    bbox_nose = nose_detector(I);
-    release(mouth_detector);
-    bbox_mouth = mouth_detector(I);
-    if isempty(bbox_nose) == 1 & isempty(bbox_mouth) == 1
-        release(detector);
-        bbox = step(detector, I);
-        a = size(bbox);
-        J = insertObjectAnnotation(I,'rectangle', bbox,'MASK WORN CORRECTLY');  
-        figure;
-        imshow(J);
-        if size(bbox) > [0,0]
-            counter = counter + 1;
-        end
-    else
-        if isempty(bbox_nose) == 0 & isempty(bbox_mouth) == 1
-            release(detector);
-            bbox = step(detector, I);
-            a = size(bbox);
-            J = insertObjectAnnotation(I,'rectangle', bbox,'MASK WORN INCORRECTLY');  
-            figure;
-            imshow(J);
-            if size(bbox) > [0,0]
-                counter = counter + 1;
-            end
+    release(detector);
+    bbox = step(detector, I);
+    s = size(bbox);
+    for i = 1: s(1)
+        release(nose_detector);
+        bbox_nose = nose_detector(I, bbox(i, :));
+        release(mouth_detector);
+        bbox_mouth = mouth_detector(I, bbox(i, :));
+        if isempty(bbox_nose) == 1 & isempty(bbox_mouth) == 1
+            J = insertObjectAnnotation(I,'rectangle', bbox(i,:),'MASK WORN CORRECTLY');  
+            I = J;
         else
-            imshow(I);
-            
+             if isempty(bbox_nose) == 0 & isempty(bbox_mouth) == 1
+                J = insertObjectAnnotation(I,'rectangle', bbox(i,:),'MASK WORN INCORRECTLY');
+                I = J;
+             end
         end
     end
+
+        figure
+       imshow(I);
 end
