9c3d9c0d8e32a04d265e12f350a688a6a1aa81d7
Sim Bordanca <sim_bordanca@yahoo.com>
2021-03-10 21:42:09 +0200

    improved

--- web_before.m
+++ web_before.m
@@ -1,25 +1,24 @@
 %cam = webcam();
-img = snapshot(cam);
+I = snapshot(cam);
 detector = vision.CascadeObjectDetector('maskdetectorr_20_60.xml');
-nose_detector = vision.CascadeObjectDetector('Nose', 'MergeThreshold', 20);
-mouth_detector = vision.CascadeObjectDetector('Mouth','MergeThreshold', 110);
+nose_detector = vision.CascadeObjectDetector('Nose', 'MergeThreshold', 20, 'UseROI', true);
+mouth_detector = vision.CascadeObjectDetector('Mouth','MergeThreshold', 110, 'UseROI', true);
 while true
-    img = snapshot(cam);    
-    bbox_nose = nose_detector(img);
-    bbox_mouth = mouth_detector(img);
-   if isempty(bbox_nose) == 1 & isempty(bbox_mouth) == 1
-        bbox = detector(img);
-        IFaces = insertObjectAnnotation(img,'rectangle', bbox,'Mask worn CORRECTLY');   
-            %figure
-        imshow(IFaces)
-   else
-        if isempty(bbox_nose) == 0 & isempty(bbox_mouth) == 1
-            bbox = detector(img);
-            IFaces = insertObjectAnnotation(img,'rectangle', bbox,'Mask worn INCORRECTLY');   
-                %figure
-            imshow(IFaces)
+    I = snapshot(cam);    
+    bbox = step(detector, I);
+    s = size(bbox);
+    for i = 1: s(1)
+        bbox_nose = nose_detector(I, bbox(i, :));
+        bbox_mouth = mouth_detector(I, bbox(i, :));
+        if isempty(bbox_nose) == 1 & isempty(bbox_mouth) == 1
+            J = insertObjectAnnotation(I,'rectangle', bbox(i,:),'MASK WORN CORRECTLY');  
+            I = J;
         else
-            imshow(img);
+             if isempty(bbox_nose) == 0 & isempty(bbox_mouth) == 1
+                J = insertObjectAnnotation(I,'rectangle', bbox(i,:),'MASK WORN INCORRECTLY');
+                I = J;
+             end
         end
     end
+       imshow(I);
 end
